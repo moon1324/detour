5d6f9f23d6a31c052a9c3d8bcf70b9b957a42895
moon1324 <michaelshouldhope@gmail.com>
2024-07-25 16:58:50 +0900

    feat: add design of signup page

--- client/src/pages/signUp/SignUp.js
+++ client/src/pages/signUp/SignUp.js
@@ -1,7 +1,53 @@
 import React from "react";
+import S from "./style";
+import Input from "../../components/input/style";
+import DetourButton from "../../components/button/DetourButton";
 
 const SignUp = () => {
-    return <div></div>;
+    return (
+        <S.Background>
+            <S.SignUpLogoWrapper>
+                <img src={process.env.PUBLIC_URL + "/images/signUp/Logo.png"} />
+            </S.SignUpLogoWrapper>
+            <S.CatchphraseWrapper>
+                <img src={process.env.PUBLIC_URL + "/images/signUp/Catchphrase.png"} />
+            </S.CatchphraseWrapper>
+            <S.BackgroundWhite></S.BackgroundWhite>
+            <S.SignUpFormContainer>
+                <S.SignUpForm>
+                    <Input variant={"white"} shape={"large"} size={"large"} color={"black"} border={"gray"} placeholder="아이디를 입력하세요" />
+                    <S.ErrorMessageWrapper>
+                        <S.ErrorMessage>아이디 또는 비밀번호를 확인해주세요!</S.ErrorMessage>
+                    </S.ErrorMessageWrapper>
+                    <Input variant={"white"} shape={"large"} size={"large"} color={"black"} border={"gray"} placeholder="비밀번호를 입력하세요" />
+                    <S.ErrorMessageWrapper>
+                        <S.ErrorMessage>아이디 또는 비밀번호를 확인해주세요!</S.ErrorMessage>
+                    </S.ErrorMessageWrapper>
+                    <Input variant={"white"} shape={"large"} size={"large"} color={"black"} border={"gray"} placeholder="이메일을 입력하세요" />
+                    <S.ErrorMessageWrapper>
+                        <S.ErrorMessage>아이디 또는 비밀번호를 확인해주세요!</S.ErrorMessage>
+                    </S.ErrorMessageWrapper>
+                    <Input variant={"white"} shape={"large"} size={"large"} color={"black"} border={"gray"} placeholder="닉네임을 입력하세요" />
+                    <S.ErrorMessageWrapper>
+                        <S.ErrorMessage>아이디 또는 비밀번호를 확인해주세요!</S.ErrorMessage>
+                    </S.ErrorMessageWrapper>
+                    <S.ForAdminLabel>
+                        <input type="checkbox" />
+                        <span>관리자</span>
+                    </S.ForAdminLabel>
+                    <Input variant={"white"} shape={"large"} size={"large"} color={"black"} border={"gray"} placeholder="관리자 키를 입력하세요" />
+                    <S.ErrorMessageWrapper>
+                        <S.ErrorMessage>아이디 또는 비밀번호를 확인해주세요!</S.ErrorMessage>
+                    </S.ErrorMessageWrapper>
+                </S.SignUpForm>
+                <S.SignUpButtonWrapper>
+                    <DetourButton variant={"gray"} shape={"small"} size={"small"} color={"black"} border={"gray"}>
+                        회원가입
+                    </DetourButton>
+                </S.SignUpButtonWrapper>
+            </S.SignUpFormContainer>
+        </S.Background>
+    );
 };
 
 export default SignUp;
--- client/src/pages/signUp/style.js
+++ client/src/pages/signUp/style.js
@@ -3,3 +3,92 @@ import theme from "../../global/theme";
 import { flexCenter, flexCenterColumn } from "../../global/common";
 
 const S = {};
+
+// 1rem = 16px
+S.Background = styled.div`
+    width: 100%;
+    height: 100%;
+    ${flexCenterColumn}
+    background-color: ${theme.PALETTE.background.yellow};
+    position: relative;
+`;
+
+S.SignUpLogoWrapper = styled.div`
+    margin-top: 4rem;
+    height: 10rem;
+    & img {
+        height: 100%;
+    }
+    margin-left: 6rem;
+`;
+
+S.CatchphraseWrapper = styled.div`
+    height: 12rem;
+    & img {
+        height: 100%;
+    }
+    margin-bottom: 4rem;
+`;
+
+S.BackgroundWhite = styled.div`
+    width: 100%;
+    background-color: ${theme.PALETTE.background.white};
+    ${flexCenterColumn}
+`;
+
+S.SignUpFormContainer = styled.div`
+    width: 28rem;
+    position: absolute;
+    top: 25rem;
+    background-color: transparent;
+    ${flexCenterColumn}
+`;
+
+S.SignUpForm = styled.form`
+    & input {
+        margin-bottom: 0.25rem;
+    }
+`;
+
+S.ErrorMessageWrapper = styled.div`
+    width: 20rem;
+    height: 1.5rem;
+    margin-bottom: 0.25rem;
+`;
+
+S.ErrorMessage = styled.span`
+    font-size: 0.75rem;
+    color: ${theme.PALETTE.error};
+    .icon {
+        font-size: 0.75rem;
+        margin-right: 0.25rem;
+        path {
+            color: ${theme.PALETTE.error};
+        }
+    }
+`;
+
+S.ForAdminLabel = styled.label`
+    width: 3.5rem;
+    height: 1rem;
+    position: absolute;
+    ${flexCenter}
+    left: 0rem;
+    bottom: 5.5rem;
+    & input {
+        margin: 0;
+        margin-right: 4px;
+    }
+    & span {
+        font-size: 12px;
+        margin-bottom: 1px;
+    }
+`;
+
+S.SignUpButtonWrapper = styled.div`
+    & button {
+        margin: 0.5rem 1.5rem;
+    }
+`;
+
+export default S;
